89f50f2fd12787911a302485ae436bb8e602270f
Mihail Radoev <mihailiradoev@gmail.com>
2024-06-07 15:16:04 +0300

    jinja_first_htmls

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers.users import users_router
 from routers.courses import courses_router
 from routers.sections import sections_router
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 
@@ -10,6 +11,7 @@ app.include_router(users_router)
 app.include_router(courses_router)
 app.include_router(sections_router)
 
+templates = Jinja2Templates(directory="templates")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- routers/users.py
+++ routers/users.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Response, status, HTTPException, Header
-
+from fastapi import APIRouter, Response, HTTPException, Header, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from common.auth import get_user_or_raise_401
 from data.models import User, LoginData
 from services import users_services, courses_services
@@ -7,27 +8,29 @@ from services.users_services import is_authenticated, give_user_info, find_by_em
 
 #Initial functionality, viewing all users not mandatory, but helpful for testing purposes
 users_router = APIRouter(prefix='/users')
+templates = Jinja2Templates(directory="templates")
 
 
-@users_router.get('/id/{user_id}')
-def show_user_by_id(user_id: int, x_token: str = Header()):
+@users_router.get("/registration", response_class=HTMLResponse)
+async def get_register_form(request: Request):
+    return templates.TemplateResponse("users/register.html", {"request": request})
+
+@users_router.get('/id/{user_id}', response_class=HTMLResponse)
+def show_user_by_id(request: Request, user_id: int, x_token: str = Header()):
     if is_authenticated(x_token):
         user_info = give_user_info(user_id)
-        if user_info:
-            return user_info
-        else:
-            raise HTTPException(status_code=404, detail="User not found")
+        return templates.TemplateResponse('users/profile.html', {"request": request, "user_info": user_info})
     else:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
 
-@users_router.get('/email/{user_email}')
-def show_user_by_email(user_email: str, x_token: str = Header()):
+@users_router.get('/email/{user_email}', response_class=HTMLResponse)
+def show_user_by_email(request: Request, user_email: str, x_token: str = Header()):
     if is_authenticated(x_token):
         user = find_by_email(user_email)
         if user:
-            user_info = give_user_info(user.id)
-            return user_info
+            user_info = give_user_info(user.user_id)
+            return templates.TemplateResponse('users/profile.html', {"request": request, "user_info": user_info})
         else:
             raise HTTPException(status_code=404, detail="User not found")
     else:
@@ -36,50 +39,56 @@ def show_user_by_email(user_email: str, x_token: str = Header()):
 
 #Guests must be able to register
 #Admins could authorize teachers' registrations(via email)
-@users_router.post('/register')
-def registration(data: User):
-    if email_exists(data.email):
+@users_router.post("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    form = await request.form()
+
+    role = form.get('role')
+    first_name = form.get('first_name')
+    last_name = form.get('last_name')
+    email = form.get('email')
+    password = form.get('password')
+
+    if email_exists(email):
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    user = create(data.role, data.first_name, data.last_name, data.password, data.email)
+    user = create(role, first_name, last_name, password, email)
     if user:
-        return {"message": "User registered successfully"}
+        return templates.TemplateResponse('users/registration_success.html', {"request": request})
     else:
         raise HTTPException(status_code=500, detail="Failed to register user")
 
 
-@users_router.post('/login')
-def login(data: LoginData):
-    user = try_login(data.email, data.password)
-    requests = None  # Initialize requests before the if block
+@users_router.post('/login', response_class=HTMLResponse)
+def login(request: Request, email: str, password: str):
+    user = try_login(email, password)
+    requests = None
     if user:
         if user.role == 'teacher':
             requests = courses_services.show_pending_requests(user.user_id)
 
-        # if login is successful, token is created
         token = create_token(user)
-        if requests:
-            return {"token": token}, "You have pending enrollment requests!"
-        return {"token": token}
+        response = templates.TemplateResponse('users/login_success.html', {"request": request, "requests": requests})
+        response.set_cookie(key="token", value=token)
+        return response
     else:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
-@users_router.post('/logout')
-def logout(response: Response):
-    # clear token from client's storage (e.g., local storage, session storage, cookies)
+@users_router.post('/logout', response_class=HTMLResponse)
+def logout(request: Request, response: Response):
+    response = templates.TemplateResponse('users/logout_success.html', {"request": request})
     response.delete_cookie("token")
-    return {"message": "Logged out successfully"}
+    return response
 
-@users_router.put('/account')
-def update_user(data: dict, x_token: str = Header()):
+@users_router.put('/account', response_class=HTMLResponse)
+def update_user(request: Request, data: dict, x_token: str = Header()):
     user = get_user_or_raise_401(x_token)
     if data.get('user_id') or data.get('role'):
         return "You cannot change your ID or role."
 
     users_services.update_user(data, user.user_id)
-
-    return "User info updated successfully!"
+    return templates.TemplateResponse('users/account_update_success.html', {"request": request})
 
 
 #Students can edit everything about their account information except their email (I assume they can't edit their id)
